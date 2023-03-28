ec7c673e1813c2325361e37e76340355b7628a53
Subhash Barela <barelasubhash16@gmail.com>
2023-03-29 01:04:30 +0530

    Display empWages using arrow function

--- Employee.js
+++ Employee.js
@@ -1,54 +1,79 @@
-const IS_PRESENT =0;
-const IS_PART_TIME =1;
-const IS_FULL_TIME =2;
-const PART_TIME_HR =4;
-const FULL_TIME_HR =8;
-const WAGE_PER_HR =20;
-const NUM_OF_WORKING_DAYS =10;
-const MAX_HRS_IN_MONTH =20;
+const Is_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HR = 4;
+const FULL_TIME_HR = 8;
+const WAGE_PER_HR = 20;
+const NUMBER_WORKING_DAYS = 10;
+const MAX_HRS = 100;
 
-function GetWorkingHrs(empCheck)
-{
-switch(empCheck)
-{
-    case IS_FULL_TIME:
-        console.log("Full time");
-    return FULL_TIME_HR;
-    break;
 
-    case IS_PART_TIME:
-        console.log("Part time ");
 
-    return PART_TIME_HR;
-    break;
 
-    default:
-        console.log("Absent ");
 
-        return 0;
 
-  }
+
+function getWorkingHours(emp_check){
+    switch(emp_check){
+        case Is_PART_TIME:
+            return PART_TIME_HR;
+            
+        case IS_FULL_TIME:
+            return FULL_TIME_HR;
+            
+        default:
+            return 0;    
+    }
 }
-function CalcDailyWage(empHrs){
-    return empHrs*WAGE_PER_HR;
+function calculateDailyWage(empHrs){
+    return empHrs * WAGE_PER_HR;
 }
 
-let empHrs=0;
-let totalEmpHrs=0;
+const findTotal = (totalVal, dailyVal)=>{
+    return totalVal+dailyVal;
+}
+
+let counnt=0;
+
+let empHrs = 0;
+let totalEmpHrs = 0;
 let totalWorkingDays=0;
-let empDailyWageArr= new Array();
 
-while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+let empDailyWageArray = new Array();
+
+let empMap = new Map();
+
+
+
+while(totalEmpHrs<=MAX_HRS && totalWorkingDays<NUMBER_WORKING_DAYS)
+{
     totalWorkingDays++;
-    let empCheck=(Math.floor(Math.random()*10)%3);    
-    empHrs= GetWorkingHrs(empCheck);
+    let emp_check = Math.floor(Math.random()*10)%3;
+    empHrs=getWorkingHours(emp_check);
     totalEmpHrs+=empHrs;
-    empDailyWageArr.push(CalcDailyWage(empHrs));
+    empDailyWageArray.push(calculateDailyWage(empHrs))
+    empMap.set(totalWorkingDays, calculateDailyWage(empHrs))
 }
-console.log(empDailyWageArr);
-let empWage=CalcDailyWage(totalEmpHrs);
-// let empWage=totalEmpHrs*WAGE_PER_HR;
-console.log("Total Days: "+ totalWorkingDays +" Total hrs: "+ totalEmpHrs +" Emp wage : "+empWage); 
 
+let totalHrs = Array.from(empMap.values()).reduce(findTotal,0);
+let totalSalary = empDailyWageArray.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
 
+console.log("uc9 " + totalHrs + " : " + totalSalary);
 
+let nonWorkingDays = new Array();
+let partWorkingDays= new Array();
+let fullWorkingDays = new Array();
+
+empMap.forEach( (value, key, map)=> {
+    if(value==8) fullWorkingDays.push(key);
+    else if(value==4) partWorkingDays.push(key);
+    else if(value==0) nonWorkingDays.push(key); 
+});
+
+console.log(fullWorkingDays);
+console.log(partWorkingDays);
+console.log(nonWorkingDays);
+
+console.log(empMap);
+
+let empWage = calculateDailyWage(totalEmpHrs);
+console.log("Wage for the month: "+empWage);
