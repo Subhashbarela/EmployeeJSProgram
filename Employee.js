baa405ac4061eed6f56c7bbbe775f5b8075bcf04
Subhash Barela <barelasubhash16@gmail.com>
2023-03-29 00:56:26 +0530

    Calculate workint dayd and hours using array

--- Employee.js
+++ Employee.js
@@ -1,3 +1,7 @@
+
+
+const IS_ABSENT =0;
+
 const Is_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HR = 4;
@@ -24,20 +28,27 @@ function getWorkingHours(emp_check){
             return 0;    
     }
 }
+function calculateDailyWage(empHrs){
+    return empHrs * WAGE_PER_HR;
+}
 
 let empHrs = 0;
 let totalEmpHrs = 0;
 let totalWorkingDays=0;
 
+let empDailyWageArray = new Array();
+
 while(totalEmpHrs<=MAX_HRS && totalWorkingDays<NUMBER_WORKING_DAYS)
 {
     totalWorkingDays++;
     let emp_check = Math.floor(Math.random()*10)%3;
-    totalEmpHrs+=getWorkingHours(emp_check);
+    empHrs=getWorkingHours(emp_check);
+    totalEmpHrs+=empHrs;
+    empDailyWageArray.push(calculateDailyWage(empHrs))
 }
 
 
+console.log(empDailyWageArray);
 
-
-let empWage = totalEmpHrs * WAGE_PER_HR;
+let empWage = calculateDailyWage(totalEmpHrs);
 console.log("Wage for the month: "+empWage);
