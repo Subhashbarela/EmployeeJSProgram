87d4a3233e9a4e2adaf39e88daae4bf1eef92bc8
Subhash Barela <barelasubhash16@gmail.com>
2023-03-29 01:02:15 +0530

    Display the Wages in Array using Seperate function

--- Employee.js
+++ Employee.js
@@ -1,50 +1,54 @@
-const Is_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HR = 4;
-const FULL_TIME_HR = 8;
-const WAGE_PER_HR = 20;
-const NUMBER_WORKING_DAYS = 10;
-const MAX_HRS = 100;
+const IS_PRESENT =0;
+const IS_PART_TIME =1;
+const IS_FULL_TIME =2;
+const PART_TIME_HR =4;
+const FULL_TIME_HR =8;
+const WAGE_PER_HR =20;
+const NUM_OF_WORKING_DAYS =10;
+const MAX_HRS_IN_MONTH =20;
 
-
-function getWorkingHours(emp_check){
-    switch(emp_check){
-        case Is_PART_TIME:
-            return PART_TIME_HR;
-            
-        case IS_FULL_TIME:
-            return FULL_TIME_HR;
-            
-        default:
-            return 0;    
-    }
-}
-function calculateDailyWage(empHrs){
-    return empHrs * WAGE_PER_HR;
-}
-
-let empHrs = 0;
-let totalEmpHrs = 0;
-let totalWorkingDays=0;
-
-let empDailyWageArray = new Array();
-
-let empMap = new Map();
-
-
-
-while(totalEmpHrs<=MAX_HRS && totalWorkingDays<NUMBER_WORKING_DAYS)
+function GetWorkingHrs(empCheck)
 {
-    totalWorkingDays++;
-    let emp_check = Math.floor(Math.random()*10)%3;
-    empHrs=getWorkingHours(emp_check);
-    totalEmpHrs+=empHrs;
-    empDailyWageArray.push(calculateDailyWage(empHrs))
-    empMap.set(totalWorkingDays, calculateDailyWage(empHrs))
+switch(empCheck)
+{
+    case IS_FULL_TIME:
+        console.log("Full time");
+    return FULL_TIME_HR;
+    break;
+
+    case IS_PART_TIME:
+        console.log("Part time ");
+
+    return PART_TIME_HR;
+    break;
+
+    default:
+        console.log("Absent ");
+
+        return 0;
+
+  }
+}
+function CalcDailyWage(empHrs){
+    return empHrs*WAGE_PER_HR;
 }
 
+let empHrs=0;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+let empDailyWageArr= new Array();
 
-console.log(empMap);
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck=(Math.floor(Math.random()*10)%3);    
+    empHrs= GetWorkingHrs(empCheck);
+    totalEmpHrs+=empHrs;
+    empDailyWageArr.push(CalcDailyWage(empHrs));
+}
+console.log(empDailyWageArr);
+let empWage=CalcDailyWage(totalEmpHrs);
+// let empWage=totalEmpHrs*WAGE_PER_HR;
+console.log("Total Days: "+ totalWorkingDays +" Total hrs: "+ totalEmpHrs +" Emp wage : "+empWage); 
 
-let empWage = calculateDailyWage(totalEmpHrs);
-console.log("Wage for the month: "+empWage);
+
+
